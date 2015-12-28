0cc8f9fddc4cc78f6b83910286d6818faa38407e
LarsHanegraaf <l.j.t.hanegraaf@student.tue.nl>
2015-12-28 16:38:58 +0100

    Add Angular 'skin'

--- public/javascripts/groupmolder.js
+++ public/javascripts/groupmolder.js
@@ -1,1 +1,14 @@
-var app = angular.module('GroupMolder', []);
+var app = angular.module('GroupMolder', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+          templateUrl: 'partials/home.html'
+        })
+        .when('/group', {
+            templateUrl: 'partials/group.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(typeof req.user.email){
-    res.render('index', { title: 'You are logged in!'});
-  }else{
+  //if(typeof req.user.email){
+  //  res.render('index', { title: 'You are logged in!'});
+  //}else{
     res.render('index', { title: 'GroupMolder' });
-  }
+  //}
 });
 
 module.exports = router;
