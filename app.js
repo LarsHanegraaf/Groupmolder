508d3529490b77e56fe5747a1819b0c4fc196603
ChristianSanders <christiansanders@live.nl>
2016-01-07 15:50:30 +0100

    Deorganized the configuration of the authentication

--- app.js
+++ app.js
@@ -20,19 +20,9 @@ var projects = require('./routes/projects');
 var authentication = require('./routes/authentication');
 
 // authenticaton modules
-var passport = require('passport');
+var passport = require("passport");
+require('./passport')(passport);
 
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findOne({'_id':id}, function(err, user){
-    done(err, user);
-  })
-});
-var LocalStrategy = require('passport-local').Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
 var fbConfig = require('./config/auth');
 
 var app = express();
@@ -57,57 +47,6 @@ app.use(session({name: 'session',
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-// authentication middleware
-
-passport.use(new LocalStrategy({
-  // override username with email
-  usernameField: 'email',
-  passwordField: 'password'
-  },
-  function(email, password, done) {
-    User.findOne({ 'local.email': email }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!bcrypt.compareSync(password, user.local.password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
-
-passport.use(new FacebookStrategy({
-    clientID: fbConfig.clientID,
-    clientSecret: fbConfig.clientSecret,
-    callbackURL: fbConfig.callbackURL
-  },
-  function(accessToken, refreshToken, profile, done){
-    User.findOne({'facebook.id': profile.id}, function(err, user){
-      if(err){
-        return done(err);
-      }
-      if(user){ // if is user is found
-        done(err, user);
-      }else{
-        var newuser = new User({
-          facebook:{
-            id: profile.id,
-            name: profile.displayName
-          }
-        });
-        newuser.save(function(err){
-          if(err) console.log(err);
-          return done(err, user);
-        });
-      }
-
-    });
-  }
-));
-
 
 // set locals variable to user in order to use it in jade
 app.use(function(req, res, next){
